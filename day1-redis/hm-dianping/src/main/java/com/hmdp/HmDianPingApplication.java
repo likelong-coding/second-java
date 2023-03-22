8b87a4ea8f8f52304a4174bb2b14d5c8c8d4b54d
likelong <likelong123>
2023-03-22 18:17:35 +0800

    发送验证码以及登录接口完善

---
+++ day1-redis/hm-dianping/src/main/java/com/hmdp/HmDianPingApplication.java
@@ -0,0 +1,13 @@
+package com.hmdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmDianPingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HmDianPingApplication.class, args);
+    }
+
+}
---
+++ day1-redis/hm-dianping/src/main/java/com/hmdp/interceptor/LoginInterceptor.java
@@ -0,0 +1,9 @@
+package com.hmdp.interceptor;
+
+/**
+ * 登录拦截器
+ * @author likelong
+ * @date 2023/3/22
+ */
+//public class LoginInterceptor extends  {
+//}

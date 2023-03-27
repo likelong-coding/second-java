47cc9a31cc3abd563d62d3e144a55def050b2a6d
likelong <likelong123>
2023-03-27 17:03:28 +0800

    基于redis实现全局id生成器

--- day1-redis/hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ day1-redis/hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,10 +1,14 @@
 package com.hmdp;
 
 import com.hmdp.service.impl.ShopServiceImpl;
+import com.hmdp.utils.RedisIdWorker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
@@ -15,6 +19,43 @@ class HmDianPingApplicationTests {
     @Test
     public void testSaveShop2Redis() {
         shopService.saveShop2Redis(1L, 10L);
+    }
+
+
+    @Resource
+    private RedisIdWorker redisIdWorker;
+
+    private final ExecutorService es = Executors.newFixedThreadPool(500);
+
+    /**
+     * 多线程测试id生成器效果
+     */
+    @Test
+    public void testIdWorker() throws InterruptedException {
+
+        CountDownLatch latch = new CountDownLatch(300);
+
+        Runnable task = () -> {
+            for (int i = 0; i < 100; i++) {
+                long id = redisIdWorker.nextId("order");
+                System.out.println("id = " + id);
+            }
+
+            // -1
+            latch.countDown();
+        };
+
+        long begin = System.currentTimeMillis();
+        // 300个线程同时跑，每个线程生成100个id
+        for (int i = 0; i < 300; i++) {
+            es.submit(task);
+        }
+
+        // 所有线程执行完后，打印耗时
+        latch.await();
+        long end = System.currentTimeMillis();
+        System.out.println("time = " + (end - begin));
+
     }
 
 }

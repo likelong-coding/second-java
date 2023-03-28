484d78f601d23b8d3b3022e0971e0a1a9d4d0158
likelong <likelong123>
2023-03-28 14:38:39 +0800

    feature: SpringBoot项目中无需添加 @EnableTransactionManagement 注解来开启事务，已经默认开启事务，在需要用到事务的方法上添加@Transactional注解即可启用事务

--- day1-redis/hm-dianping/src/main/java/com/hmdp/HmDianPingApplication.java
+++ day1-redis/hm-dianping/src/main/java/com/hmdp/HmDianPingApplication.java
@@ -3,11 +3,14 @@ package com.hmdp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+/**
+ * @author likelong
+ */
 @SpringBootApplication
 @EnableAspectJAutoProxy(exposeProxy = true)
-@EnableTransactionManagement
+// SpringBoot项目中无需添加该注解开启事务，已经默认开启
+//@EnableTransactionManagement
 public class HmDianPingApplication {
 
     public static void main(String[] args) {

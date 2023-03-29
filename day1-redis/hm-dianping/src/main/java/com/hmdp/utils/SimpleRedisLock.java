d641f87b458777d657c710ef7d7b78e1bd6f46dc
likelong <247907lkl>
2023-03-30 00:07:18 +0800

    基于lua脚本实现判断锁、删除锁原子性操作

--- day1-redis/hm-dianping/src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ day1-redis/hm-dianping/src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,8 +1,11 @@
 package com.hmdp.utils;
 
 import cn.hutool.core.lang.UUID;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -16,6 +19,13 @@ public class SimpleRedisLock implements ILock {
 
     private static final String KEY_PREFIX = "lock:";
     private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
 
     public SimpleRedisLock(String name, StringRedisTemplate stringRedisTemplate) {
         this.name = name;
@@ -25,7 +35,7 @@ public class SimpleRedisLock implements ILock {
     @Override
     public boolean tryLock(long timeoutSec) {
         // 获取线程标示
-        String threadId = ID_PREFIX + Thread.currentThread().getId() + "";
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
         // 获取锁
         Boolean success = stringRedisTemplate.opsForValue()
                 .setIfAbsent(KEY_PREFIX + name, threadId, timeoutSec, TimeUnit.SECONDS);
@@ -33,6 +43,15 @@ public class SimpleRedisLock implements ILock {
     }
 
     @Override
+    public void unlock() {
+        // 调用Lua脚本 执行原子操作
+        stringRedisTemplate.execute(
+                UNLOCK_SCRIPT,
+                Collections.singletonList(KEY_PREFIX + name),
+                ID_PREFIX + Thread.currentThread().getId());
+    }
+
+/*    @Override
     public void unlock() {
 
         // 获取线程标识，判断线程标识是否一致
@@ -47,5 +66,5 @@ public class SimpleRedisLock implements ILock {
 
         // 否则释放锁
         stringRedisTemplate.delete(KEY_PREFIX + name);
-    }
+    }*/
 }

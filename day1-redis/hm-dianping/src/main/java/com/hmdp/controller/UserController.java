74105a5ac73a9db5fcf3ae808dc064402e086064
likelong <247907lkl>
2023-04-22 23:00:50 +0800

    feat: BitMap实现签到功能、连续签到统计

--- day1-redis/hm-dianping/src/main/java/com/hmdp/controller/UserController.java
+++ day1-redis/hm-dianping/src/main/java/com/hmdp/controller/UserController.java
@@ -96,4 +96,20 @@ public class UserController {
         return Result.ok(userDTO);
     }
 
+    /**
+     * 签到
+     */
+    @PostMapping("/sign")
+    public Result sign(){
+        return userService.sign();
+    }
+
+    /**
+     * 连续签到统计
+     */
+    @GetMapping("/sign/count")
+    public Result signCount(){
+        return userService.signCount();
+    }
+
 }

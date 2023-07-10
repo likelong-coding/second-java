e3ddbd0430fbf17afbd6e4b656a282b1bf931f5e
likelong <likelonglkl@163.com>
2023-07-11 00:07:17 +0800

    feat: BeanFactoryPostProcessor（自定义PostProcessor）解析Bean定义

---
+++ day2-spring/spring-demo/src/main/java/com/lkl/spring/chapter5/mapper/Mapper1.java
@@ -0,0 +1,7 @@
+package com.lkl.spring.chapter5.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface Mapper1 {
+}
---
+++ day2-spring/spring-demo/src/main/java/com/lkl/spring/chapter5/mapper/Mapper3.java
@@ -0,0 +1,4 @@
+package com.lkl.spring.chapter5.mapper;
+
+public class Mapper3 {
+}

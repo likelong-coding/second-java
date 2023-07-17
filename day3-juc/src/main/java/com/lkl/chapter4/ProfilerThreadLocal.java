c53d15dc4b71c3a5e3b8c6ec45a824f0b5a2db97
likelong <likelonglkl@163.com>
2023-07-17 23:12:40 +0800

    feat: ThreadLocal注释修改

--- day3-juc/src/main/java/com/lkl/chapter4/ProfilerThreadLocal.java
+++ day3-juc/src/main/java/com/lkl/chapter4/ProfilerThreadLocal.java
@@ -3,7 +3,7 @@ package com.lkl.chapter4;
 import java.util.concurrent.TimeUnit;
 
 /**
- * ThreadLocal 以线程为key ， 泛型类型为value （线程隔离）
+ * ThreadLocal 以ThreadLocal对象key ， 泛型类型为value （线程隔离）
  */
 public class ProfilerThreadLocal {
     // 第一次get()方法调用时会进行初始化（如果set方法没有调用），每个线程会调用一次

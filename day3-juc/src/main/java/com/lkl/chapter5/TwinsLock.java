ad6bb7110f405ebd39f816e5ac1e2f8b9fa3600a
likelong <likelonglkl@163.com>
2023-07-23 00:13:05 +0800

    feat: 自定义共享锁

---
+++ day3-juc/src/main/java/com/lkl/chapter5/TwinsLock.java
@@ -0,0 +1,73 @@
+package com.lkl.chapter5;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+/**
+ * 同时两个线程可以获取同步状态
+ */
+public class TwinsLock implements Lock {
+    private final Sync sync = new Sync(2);
+
+    private static final class Sync extends AbstractQueuedSynchronizer {
+        Sync(int count) {
+            if (count <= 0) {
+                throw new IllegalArgumentException("count must large than zero.");
+            }
+            setState(count);
+        }
+
+        public int tryAcquireShared(int reduceCount) {
+            // 自旋 + CAS
+            for (; ; ) {
+                int current = getState();
+                int newCount = current - reduceCount;
+                if (newCount < 0 || compareAndSetState(current, newCount)) {
+                    return newCount;
+                }
+            }
+        }
+
+        public boolean tryReleaseShared(int returnCount) {
+            // 自旋 + CAS
+            for (; ; ) {
+                int current = getState();
+                int newCount = current + returnCount;
+                if (compareAndSetState(current, newCount)) {
+                    return true;
+                }
+            }
+        }
+    }
+
+    @Override
+    public void lockInterruptibly() throws InterruptedException {
+
+    }
+
+    @Override
+    public boolean tryLock() {
+        return false;
+    }
+
+    @Override
+    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+        return false;
+    }
+
+    @Override
+    public Condition newCondition() {
+        return null;
+    }
+
+    // 主要看下面两个方法
+    public void lock() {
+        sync.acquireShared(1);
+    }
+
+    public void unlock() {
+        sync.releaseShared(1);
+    }
+}
